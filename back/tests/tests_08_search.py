4e7828740acf74786e64838ac9eed3b3c20e1c43
Karocyt/kevazoul <azoulay.kevin+robot@gmail.com>
2020-10-16 23:58:10 +0200

    search working for age and tags

--- back/tests/tests_08_search.py
+++ back/tests/tests_08_search.py
@@ -5,4 +5,31 @@ from .utils import (signup, login, validate, create, update, delete,
                     report, reset)
 
 def test_search():
-    pass
+    login(user1)
+    response = user1["session"].get(f"{url}/search",
+                                    json={'age': {'min': 18, 'max': 27}})
+    assert response.status_code == 200
+    assert response.json()["users"][0]["age"] < 27
+
+    response = user1["session"].get(f"{url}/search",
+                                    json={'tags': ['street']})
+    assert response.status_code == 200
+    print(response.json()["users"], flush=True)
+    profile = user1["session"].get(f"{url}/users/{response.json()['users'][0]['id']}").json()
+    print(profile, flush=True)
+    assert 'street' in profile["tags"]
+
+    response = user1["session"].get(f"{url}/search",
+                                    json={'tags': ['street', 'rue']})
+    assert response.status_code == 200
+    profile = user1["session"].get(f"{url}/users/{response.json()['users'][0]['id']}").json()
+    assert 'street' in profile["tags"] or 'rue' in profile["tags"]
+
+
+    response = user1["session"].get(f"{url}/search",
+                                    json={
+                                            'age': {'min': 18, 'max': 99},
+                                            'tags': ['rue', 'de']})
+    assert response.status_code == 200
+    profile = user1["session"].get(f"{url}/users/{response.json()['users'][0]['id']}").json()
+    assert 'rue' in profile["tags"]
